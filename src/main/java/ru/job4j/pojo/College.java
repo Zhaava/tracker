cbc7dd32bca4807f302ddb7c05d5b1cfb020cea4
Alexey <zhavoronkov52@gmail.com>
2021-03-12 19:17:15 +0300

    2. Модель данных. [#372436]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -9,9 +9,9 @@ public class College {
         student.setGroup(202);
         student.setDateOfAdmission(new Date(2000, 5, 6));
 
-        System.out.println(student.getName() + "\ngroup: "
-                + student.getGroup() + "\nyear of admission: "
-                + student.getDateOfAdmission().getYear() + "\nmonth of admission: "
+        System.out.println(student.getName() + System.lineSeparator() + "group: "
+                + student.getGroup() + System.lineSeparator() + "year of admission: "
+                + student.getDateOfAdmission().getYear() + System.lineSeparator() + "month of admission: "
                 + student.getDateOfAdmission().getMonth());
     }
 }
