bcc1ec26187b286239e343835ed85543f4a54a3c
Alexey <zhavoronkov52@gmail.com>
2021-04-25 11:14:54 +0300

    3.2. За нужно приведение типов [#372374]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,25 +1,8 @@
 package ru.job4j.poly;
 
-public class Bus implements Transport {
+public class Bus implements Vehicle {
     @Override
     public void move() {
-        System.out.println("Bus is moving");
-    }
-
-    @Override
-    public void getPassengers(int amountOfPassengers) {
-        System.out.println(amountOfPassengers + " passengers on board");
-    }
-
-    @Override
-    public int refuel(int amountOfFuel) {
-        return amountOfFuel * 30;
-    }
-
-    public static void main(String[] args) {
-        Transport bus = new Bus();
-        bus.move();
-        bus.getPassengers(15);
-        System.out.println("Refuel costs " + bus.refuel(20) + " rubles");
+        System.out.println("Bus is moving on road.");
     }
 }
