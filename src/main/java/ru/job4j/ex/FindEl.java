19bcaf9d1ebb26d14615fe73677b401942582581
Alexey <zhavoronkov52@gmail.com>
2021-06-07 17:01:04 +0300

    0.4. Пользовательские исключения. [#219367 #165558]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,19 +4,20 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if (value[i] == null) {
-                throw new ElementNotFoundException("Elements in array could not be null");
-            }
-            if (Integer.parseInt(value[i]) == Integer.parseInt(key)) {
+            if (key.equals(value[i])) {
                 rsl = i;
+                break;
             }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Elements in array could not be found");
         }
         return rsl;
     }
 
     public static void main(String[] args) {
-        String[] testArray = {"5", "7", null, "12"};
-        String key = "12";
+        String[] testArray = {"5", "7", "2", "4"};
+        String key = "1";
         try {
             System.out.println(indexOf(testArray, key));
         } catch (ElementNotFoundException e) {
