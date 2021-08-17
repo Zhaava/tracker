d1fb285062718b730b9313eddf04f7156cc01780
Alexey <zhavoronkov52@gmail.com>
2021-08-17 19:55:08 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,13 +1,23 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.Objects.compare;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
+                .collect(Collectors.toList());
+    }
+
+    public List<Address> collectUniqueAddress(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .sorted((s1,s2) -> s1.getCity().compareTo(s2.getCity()))
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -23,4 +23,24 @@ public class ProfilesTest {
         List<Address> rsl = profiles.collect(persons);
         assertThat(expected,is(rsl));
     }
+
+    @Test
+    public void whenListDoubleAddress() {
+        List<Profile> persons = List.of(
+                new Profile(new Address("london", "baker", 4,5)),
+                new Profile(new Address("moscow", "lenin", 5,4)),
+                new Profile(new Address("aprelevka", "aprel", 4,5)),
+                new Profile(new Address("paris", "versal", 2,7)),
+                new Profile(new Address("moscow", "lenin", 5,4))
+        );
+        List<Address> expected = List.of(
+                new Address("aprelevka", "aprel", 4,5),
+                new Address("london", "baker", 4,5),
+                new Address("moscow", "lenin", 5,4),
+                new Address("paris", "versal", 2,7)
+        );
+        Profiles profiles = new Profiles();
+        List<Address> rsl = profiles.collectUniqueAddress(persons);
+        assertThat(expected,is(rsl));
+    }
 }
